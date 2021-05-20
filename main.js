efecef4387df9e51edc3e0b3ee077693bafe5856
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-05-20 16:26:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+  canvas = createCanvas(300, 280);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+function modelLoaded() {
+  console.log(modelLoaded);
+}
+function draw() {
+  image(video,0,0,300,300);
+  classifier.classify(video ,gotResult);
+}
+function gotResult(results ,error) {
+  if(error){
+    console.error(error);
+  }else{
+    console.log(results)
+    document.getElementById("result_span_object").innerHTML = results[0].label;
+    document.getElementById("result_span_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
+
+
+
